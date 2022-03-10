943d386cf65d65431d18ddbd167546d8e66ae92f
pc <pc@DESKTOP-NR2QRMV>
2022-03-10 20:09:26 +0300

    Proje dosyası ekle.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Metotlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Product Urun1 = new Product();
+            Urun1.Adi = "Elma";
+            Urun1.Acıklama = "Amasya Elması";
+            Urun1.Fiyati = 15;
+
+            Product Urun2 = new Product();
+            Urun2.Adi = "Karpuz";
+            Urun2.Fiyati = 80;
+            Urun2.Acıklama = "Diyarbakır Karpuzu";
+
+            Product[] Urunler = new Product[] { Urun1, Urun2 };
+
+            foreach ( var  Urun in Urunler)
+            {
+                Console.WriteLine(Urun.Adi +": "+ Urun.Acıklama +", Fiyatı "+ Urun.Fiyati);
+                Console.WriteLine("-------------------------------------");
+            }
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(Urun1);
+
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Metotlar
+{
+    internal class SepetManager
+    {
+        public void Ekle(Product urun)
+        {
+            Console.WriteLine("Sepete Eklendi : "+ urun.Adi);
+        }
+    }
+}
